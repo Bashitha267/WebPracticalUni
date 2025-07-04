623b100240b5724a26e01d6cd023373de48137a5
DCS User <DCSUser@cn.lan>
2025-07-04 12:05:16 +0530

    Edited

--- Portfolio/Portfolio/index.js
+++ Portfolio/Portfolio/index.js
@@ -3,7 +3,13 @@ let name;
 let email;
 let message;
 
-
+let mess=[
+    {
+        name,
+        email,
+        message,
+    }
+]
 const container=document.getElementById('form-status')
 const div=document.createElement('div');
 const form=document.getElementById('contact-form')
@@ -33,8 +39,20 @@ container.append(div)
 const skillList=document.getElementById('skills-list');
 const list=document.createElement('li')
 const list2=document.createElement('li')
+const list3=document.createElement('li')
 
 list.innerText="React"
 skillList.append(list)
 list2.innerText="Node"
 skillList.append(list2)
+list3.innerText="React Native"
+skillList.append(list3)
+
+
+
+const projects=document.getElementById('projects-container');
+const projectlist=document.createElement('div')
+projectlist.style="padding:15px"
+projectlist.innerHTML="<div> Fashora</div> <div> Fashora</div> <div> Fashora</div>"
+
+projects.append(projectlist)
